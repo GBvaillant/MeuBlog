eafac443f89c6d1a262187e26797b34dafff4946
GBvaillant <vaillantgabriel@gmail.com>
2023-05-10 11:38:05 -0300

    Primeiro commit

---
+++ src/dbPost.js
@@ -0,0 +1,27 @@
+const gerador = {
+    titulo: "Gerador",
+    desc: "Um gerador de senhas aleatórias feito com HTML, CSS e JavaScript"
+}
+document.getElementById('titulo-gerador').innerHTML = gerador.titulo
+document.getElementById('desc-gerador').innerHTML = gerador.desc
+
+const relogio = {
+    titulo: 'Relógio digital',
+    desc: 'Relógio digital é um pequeno projeto de FrontEnd, feito com HTML, CSS e JavaScript.'
+}
+document.getElementById('titulo-relogio').innerHTML = relogio.titulo
+document.getElementById('desc-relogio').innerHTML = relogio.desc
+
+const sistemasol = {
+    titulo: 'Sistema Solar',
+    desc: 'Um projeto feito focado no frontend usando o JavaScript para criar sliders e listar os planetas do sistema solar com uma breve descrição.'
+}
+document.getElementById('titulo-sistemasol').innerHTML = sistemasol.titulo
+document.getElementById('desc-sistemasol').innerHTML = sistemasol.desc
+
+const tellogin = { 
+    titulo:'Tela de login/registro',
+    desc:'Um projeto de tela de login e registro, feito com HTML, CSS e JavaScript. Este projeto tinha o intuito de consumir uma API, pois eu estava começando a estudar NodeJS'
+}
+document.getElementById('titulo-tellogin').innerHTML = tellogin.titulo
+document.getElementById('desc-tellogin').innerHTML = tellogin.desc
