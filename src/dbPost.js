b70d71dad614619c68ea42e5ff864cf7f3a890f3
GBvaillant <vaillantgabriel@gmail.com>
2023-05-16 20:27:02 -0300

    commit atualização

--- src/dbPost.js
+++ src/dbPost.js
@@ -19,9 +19,18 @@ const sistemasol = {
 document.getElementById('titulo-sistemasol').innerHTML = sistemasol.titulo
 document.getElementById('desc-sistemasol').innerHTML = sistemasol.desc
 
-const tellogin = { 
-    titulo:'Tela de login/registro',
-    desc:'Um projeto de tela de login e registro, feito com HTML, CSS e JavaScript. Este projeto tinha o intuito de consumir uma API, pois eu estava começando a estudar NodeJS'
+const tellogin = {
+    titulo: 'Tela de login/registro',
+    desc: 'Um projeto de tela de login e registro, feito com HTML, CSS e JavaScript. Este projeto tinha o intuito de consumir uma API, pois eu estava começando a estudar NodeJS'
 }
 document.getElementById('titulo-tellogin').innerHTML = tellogin.titulo
 document.getElementById('desc-tellogin').innerHTML = tellogin.desc
+
+const jumbtxt = {
+    titulo: 'Gabriel Vaillant',
+    desc: 'Olá, fiz esse site para contar um pouco sobre mim, meus estudos e mostrar alguns dos meus projetos, espero que gostem',
+    pub: 'Me chamo Gabriel Vaillant, moro no Rio de Janeiro e estudo programação Por muito tempo eu não sabia o que fazer da vida até que descobri a programação, e já que sempre fui muito ligado em computadores, video games e etc então resolvi me arriscar nesse mundo Hoje faço faculdade de Sistemas da Informação além de estudar desenvolvimento web, estou no meu 3º período atualmente e buscando o meu primeiro emprego na área da tecnologia Sempre gostei tanto do FrontEnd quanto do BackEnd e estou buscando aprender cada vez mais.'
+}
+document.getElementById('titulo-jumb').innerHTML = jumbtxt.titulo
+document.getElementById('desc-jumb').innerHTML = jumbtxt.desc
+document.getElementById('pub-jumb').innerHTML = jumbtxt.pub
