d654b398f8f6317d8ea6e4a6c848002b23d13ffc
GBvaillant <vaillantgabriel@gmail.com>
2023-05-21 19:47:04 -0300

    Commit deploy

--- src/dbPost.js
+++ src/dbPost.js
@@ -6,8 +6,8 @@ document.getElementById('titulo-gerador').innerHTML = gerador.titulo
 document.getElementById('desc-gerador').innerHTML = gerador.desc
 
 const relogio = {
-    titulo: 'Relógio digital',
-    desc: 'Relógio digital é um pequeno projeto de FrontEnd, feito com HTML, CSS e JavaScript.'
+    titulo: 'Relógio Digital',
+    desc: 'Relógio digital é um projeto de FrontEnd, feito com HTML, CSS e JavaScript.'
 }
 document.getElementById('titulo-relogio').innerHTML = relogio.titulo
 document.getElementById('desc-relogio').innerHTML = relogio.desc
@@ -21,7 +21,7 @@ document.getElementById('desc-sistemasol').innerHTML = sistemasol.desc
 
 const tellogin = {
     titulo: 'Tela de login/registro',
-    desc: 'Um projeto de tela de login e registro, feito com HTML, CSS e JavaScript. Este projeto tinha o intuito de consumir uma API, pois eu estava começando a estudar NodeJS'
+    desc: 'Um projeto de tela de login e registro, feito com HTML, CSS e JavaScript.'
 }
 document.getElementById('titulo-tellogin').innerHTML = tellogin.titulo
 document.getElementById('desc-tellogin').innerHTML = tellogin.desc
@@ -29,8 +29,23 @@ document.getElementById('desc-tellogin').innerHTML = tellogin.desc
 const jumbtxt = {
     titulo: 'Gabriel Vaillant',
     desc: 'Olá, fiz esse site para contar um pouco sobre mim, meus estudos e mostrar alguns dos meus projetos, espero que gostem',
-    pub: 'Me chamo Gabriel Vaillant, moro no Rio de Janeiro e estudo programação Por muito tempo eu não sabia o que fazer da vida até que descobri a programação, e já que sempre fui muito ligado em computadores, video games e etc então resolvi me arriscar nesse mundo Hoje faço faculdade de Sistemas da Informação além de estudar desenvolvimento web, estou no meu 3º período atualmente e buscando o meu primeiro emprego na área da tecnologia Sempre gostei tanto do FrontEnd quanto do BackEnd e estou buscando aprender cada vez mais.'
+    pub: 'Me chamo Gabriel Vaillant, moro no Rio de Janeiro e estudo programação. Já fiz dois semestres de análise e desenvolvimento de sistemas em 2019, porém me encontrei na programação só depois, já que sempre fui muito ligado em computadores, video games e etc,  resolvi me arriscar nesse mundo. Hoje faço faculdade de Sistemas da Informação, além de estudar desenvolvimento web, estou no meu 3º período atualmente e buscando o meu primeiro emprego na área da tecnologia. Gosto tanto do FrontEnd quanto do BackEnd, estou focado em estudar e aprender cada vez mais.'
 }
 document.getElementById('titulo-jumb').innerHTML = jumbtxt.titulo
 document.getElementById('desc-jumb').innerHTML = jumbtxt.desc
 document.getElementById('pub-jumb').innerHTML = jumbtxt.pub
+
+const apiNaruto = {
+    titulo: 'Api Naruto',
+    desc: 'Quem nunca ouviu falar de Naruto não é mesmo? Essa API feita com NodeJs mostra frases marcantes do anime Naruto.'
+}
+document.getElementById('naruto-titulo').innerHTML = apiNaruto.titulo
+document.getElementById('naruto-desc').innerHTML = apiNaruto.desc
+
+const apiCrud = { 
+    titulo: 'CRUD NodeJs',
+    desc: 'Crud feito com NodeJs + banco de dados online com mongodb atlas.'
+}
+
+document.getElementById('titulo-crud').innerHTML = apiCrud.titulo
+document.getElementById('desc-crud').innerHTML = apiCrud.desc
