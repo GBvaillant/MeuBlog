92ca2a08e4ac9587154ad5bdd07f0ee879580b1f
GBvaillant <vaillantgabriel@gmail.com>
2023-07-28 20:20:23 -0300

    Add novos projetos, removendo outros e add novas tecnologias(logos)

--- src/dbPost.js
+++ src/dbPost.js
@@ -12,19 +12,34 @@ const relogio = {
 document.getElementById('titulo-relogio').innerHTML = relogio.titulo
 document.getElementById('desc-relogio').innerHTML = relogio.desc
 
-const sistemasol = {
-    titulo: 'Sistema Solar',
-    desc: 'Um projeto feito focado no frontend usando o JavaScript para criar sliders e listar os planetas do sistema solar com uma breve descrição.'
-}
-document.getElementById('titulo-sistemasol').innerHTML = sistemasol.titulo
-document.getElementById('desc-sistemasol').innerHTML = sistemasol.desc
+// const sistemasol = {
+//     titulo: 'Sistema Solar',
+//     desc: 'Um projeto feito focado no frontend usando o JavaScript para criar sliders e listar os planetas do sistema solar com uma breve descrição.'
+// }
+// document.getElementById('titulo-sistemasol').innerHTML = sistemasol.titulo
+// document.getElementById('desc-sistemasol').innerHTML = sistemasol.desc
 
-const tellogin = {
-    titulo: 'Tela de login/registro',
-    desc: 'Um projeto de tela de login e registro, feito com HTML, CSS e JavaScript.'
+// const tellogin = {
+//     titulo: 'Tela de login/registro',
+//     desc: 'Um projeto de tela de login e registro, feito com HTML, CSS e JavaScript.'
+// }
+// document.getElementById('titulo-tellogin').innerHTML = tellogin.titulo
+// document.getElementById('desc-tellogin').innerHTML = tellogin.desc
+
+const listaTarefas = {
+    titulo: 'Lista de tarefas',
+    desc: 'Lista de tarefas desenvolvida em React, consumindo uma api própria feita em NodeJs, com funcionaliades de listar tarefas, adicionar, excluir, completar, desfazer tarefa, ordenar por completas e incompletas e pesquisar tarefas. '
 }
-document.getElementById('titulo-tellogin').innerHTML = tellogin.titulo
-document.getElementById('desc-tellogin').innerHTML = tellogin.desc
+document.getElementById('titulo-tarefas').innerHTML = listaTarefas.titulo
+document.getElementById('desc-tarefas').innerHTML = listaTarefas.desc
+
+const projetos = {
+    titulo: 'Gerenciador de projetos',
+    desc: 'Gerenciador de projetos desenvolvido em react, com backend feito em json server e funcionalidades como: criação do projeto com título, descrição e categoria, editar projeto, gerenciar o custo, adicionar serviços, editar serviços e validações para não ultrapassar o limite financeiro do projeto.'
+}
+document.getElementById('titulo-projetos').innerHTML = projetos.titulo
+document.getElementById('desc-projetos').innerHTML = projetos.desc
+
 
 const jumbtxt = {
     titulo: 'Gabriel Vaillant',
@@ -49,3 +64,11 @@ const apiCrud = {
 
 document.getElementById('titulo-crud').innerHTML = apiCrud.titulo
 document.getElementById('desc-crud').innerHTML = apiCrud.desc
+
+const apiTarefas = {
+    titulo: 'API lista de tarefas',
+    desc: 'Api feita em NodeJs com banco de dados MongoDb atlas, para lista de tarefas com funcionalidades de adicionar tarefas, listar tarefas, excluir tarefas e completar/descompletar tarefas'
+}
+
+document.getElementById('titulo-apiTarefas').innerHTML = apiTarefas.titulo
+document.getElementById('desc-apiTarefas').innerHTML = apiTarefas.desc
